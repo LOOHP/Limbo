3d0c00bd73f6836c9e866023f1485cbf579b3f4c
LOOHP <jamesloohp@gmail.com>
2021-04-19 13:26:55 +0800

    Revert "Suppress socket errors"
    This reverts commit 728306ec8894f395fda455284dd2dbced163cb96.

--- src/main/java/com/loohp/limbo/server/ClientConnection.java
+++ src/main/java/com/loohp/limbo/server/ClientConnection.java
@@ -451,7 +451,7 @@ public class ClientConnection extends Thread {
 
 	    	}
 
-    	} catch (Exception e) {}
+    	} catch (Exception e) {e.printStackTrace();}
 
     	try {
 			client_socket.close();
