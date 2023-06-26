8083e7b1b081d5098adef7f84c7a19ffd52546b3
LOOHP <jamesloohp@gmail.com>
2023-06-26 17:01:21 +0800

    Minecraft 1.20.1

--- src/main/java/com/loohp/limbo/Limbo.java
+++ src/main/java/com/loohp/limbo/Limbo.java
@@ -139,7 +139,7 @@ public final class Limbo {
 	
 	//===========================
 	
-	public final String SERVER_IMPLEMENTATION_VERSION = "1.20";
+	public final String SERVER_IMPLEMENTATION_VERSION = "1.20.1";
 	public final int SERVER_IMPLEMENTATION_PROTOCOL = 763;
 	public final String LIMBO_IMPLEMENTATION_VERSION;
 	
